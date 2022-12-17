9a37bceff47c05002a3222dd80373d9c26fd112c
Ty Pederson <62142901+TyPe23@users.noreply.github.com>
2022-12-16 23:03:27 -0600

    Created player character with hands that animate, needs to be tested

---
+++ CapstoneEscapeRoom/Assets/Models/Scripts/Hand.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Hand : MonoBehaviour
+{
+    public float speed;
+
+    Animator animator;
+    SkinnedMeshRenderer mesh;
+
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator= GetComponent<Animator>();
+        mesh = GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        }
+        
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        mesh.enabled = !mesh.enabled;
+    }
+}
