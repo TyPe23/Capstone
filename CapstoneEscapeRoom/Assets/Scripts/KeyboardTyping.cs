304f646d94dcb701afb33603c30c1dbf65a8d73a
MarydeM <maryevelyndemarigny@gmail.com>
2023-01-04 22:15:23 -0600

    keyboard funciton is working, and the keyboard is being made

--- CapstoneEscapeRoom/Assets/Scripts/KeyboardTyping.cs
+++ CapstoneEscapeRoom/Assets/Scripts/KeyboardTyping.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class KeyboardTyping : MonoBehaviour {
 
     string word = null;
     int wordIndex = 0;
     string alpha;
-    public Text input = null;
+    public TMP_Text input = null;
+    
 
     public void typingFunct(string letter) {
         wordIndex++;
         word = word + letter;
-        input.text = word;
+        input.text = word; // send to text mesh pro
     }
 }
