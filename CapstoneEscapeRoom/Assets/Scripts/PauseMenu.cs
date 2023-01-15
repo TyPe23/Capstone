366ad9901bc9a35b52ccde8b51a09f29d425b032
MatthewT <61696310+firerexman007@users.noreply.github.com>
2023-01-14 22:28:47 -0600

    Moving Files and pause menu setup
    Changed where some items in folder are located to start cleaning up the folders (will need to finish as a group). Started working on Pause menu and added to levelSelection however not tested.

---
+++ CapstoneEscapeRoom/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+// Name: Matthew Tucker 
+// Date: 1/14/23
+// Discription: Handles pause menue pull up and function 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    // controler 
+    public GameObject wristUI;
+    //  bool about if active 
+    public bool activeWristUI = true; 
+    
+    void Start() // on start up 
+    {
+        DisplayWristUI(); 
+    }
+
+    // change if displaying or not
+    public void DisplayWristUI()
+    {
+        if(activeWristUI)
+        {
+            wristUI.SetActive(false);
+            activeWristUI = false;
+        }
+        else if (!activeWristUI)
+        {
+            wristUI.SetActive(true);
+            activeWristUI = true;
+        }
+    }
+    // pause button if button clicked and removal of pause menue if clicked 
+    public void PauseButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            DisplayWristUI();
+        }
+    }
+
+    public void restartLevel() // restart level 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void exitLevel() // return to level selection function 
+    {
+        SceneManager.LoadScene(1); // retunrs to level selection - NOTE: Might want to add a failed and time in game if done 
+    }
+
+    public void exitGame() // leave game 
+    {
+        Application.Quit();
+    }
+}
