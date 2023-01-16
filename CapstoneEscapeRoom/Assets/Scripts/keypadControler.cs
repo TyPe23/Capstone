d58c92417e372e6571d47b811f6ac1cd3ee5a837
MatthewT <61696310+firerexman007@users.noreply.github.com>
2023-01-15 22:45:11 -0600

    Keypad
    Setup, made, and connected keypad to indicate when correct or incorrect.

---
+++ CapstoneEscapeRoom/Assets/Scripts/keypadControler.cs
@@ -0,0 +1,65 @@
+// Name: Matthew Tucker
+// Data: 1/15/2023
+// Description: Handles keypad input and checking 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime;
+using Unity.VisualScripting;
+
+public class keypadControler : MonoBehaviour
+{
+    string textOut = null; // output 
+    public int wordIndex = 0;// size of output 
+    public TMP_Text output = null; // text display
+    public GameObject inputField; // where to get correct number 
+    string[] inputs = { "ent", "0", "del", "1", "2", "3", "4", "5", "6", "7", "8", "9" }; // valid inputs (never used) 
+    private string textIn; // correct input - make public for testing else private 
+
+    public Material[] Materials; // materials list / colors 
+    public Renderer rend;// renders 
+
+    void Start() { // get input 
+        textIn = inputField.GetComponent<TMP_Text>().text;  
+    }   
+    
+    
+
+    public void typingFunct(string letter) // input letters 
+    {
+        wordIndex++;
+        textOut = textOut + letter;
+        output.text = textOut; // send to text mesh pro
+    }
+
+
+    public void backspaceFunct()
+    {
+        //only erase a character if there are characters to erase
+        if (wordIndex > 0)
+        {
+            //subtract a character from the word
+            textOut = textOut.Substring(0, wordIndex - 1);
+            wordIndex--;
+            output.text = textOut;
+        }
+    }
+
+    public void enterFunct() // enter and check if correct 
+    {
+       if(textOut == textIn) // if correct 
+        {
+            rend.enabled = true;
+            rend.sharedMaterial = Materials[0];
+        }
+        else // if wrong 
+        {
+            rend.enabled = true;
+            rend.sharedMaterial = Materials[1];
+        }
+    }
+
+}
