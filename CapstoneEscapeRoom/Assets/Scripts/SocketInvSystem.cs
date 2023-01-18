331b1ea5b7295092ae571b1654ca644c5c59c500
Ty Pederson <ty23117@gmail.com>
2023-01-17 18:26:35 -0600

    Inventory system finished

---
+++ CapstoneEscapeRoom/Assets/Scripts/SocketInvSystem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SocketInvSystem : MonoBehaviour
+
+{
+    public GameObject HMD;
+
+    private Vector3 _currentHMDlocalPosition;
+    private Quaternion _currentHMDRotation;
+    void Update()
+    {
+        _currentHMDlocalPosition = HMD.transform.localPosition;
+        _currentHMDRotation = HMD.transform.rotation;
+        UpdateSocketInventory();
+    }
+
+
+    private void UpdateSocketInventory()
+    {
+        transform.localPosition = new Vector3(_currentHMDlocalPosition.x, 0, _currentHMDlocalPosition.z);
+        transform.rotation = new Quaternion(transform.rotation.x, _currentHMDRotation.y, transform.rotation.z, _currentHMDRotation.w);
+    }
+}
