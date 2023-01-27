eb5bd83cebc24c9f236b2642ea397e65f7243b14
MarydeM <maryevelyndemarigny@gmail.com>
2023-01-26 23:18:28 -0600

    terminal is working somewhat, but typing overwrites existing text

--- CapstoneEscapeRoom/Assets/Scripts/KeyboardTyping.cs
+++ CapstoneEscapeRoom/Assets/Scripts/KeyboardTyping.cs
@@ -6,25 +6,25 @@ using TMPro;
 
 public class KeyboardTyping : MonoBehaviour {
 
-    string word = null;
-    int wordIndex = 0;
+    public string word = null;
+    public int wordIndex = 0;
     public TMP_Text output = null;
     bool shift = false;
     IDictionary<string, string> shiftDictionary = new Dictionary<string, string>() {
-        {"a", "A"}, {"b", "B"}, {"c", "C" }, {"d", "D"}, 
+        {"a", "A"}, {"b", "B"}, {"c", "C" }, {"d", "D"},
         {"e", "E"}, {"f", "F"}, {"g", "G"}, {"h", "H"},
         {"i", "I"}, {"j", "J" }, {"k", "K"}, {"l", "L"},
         {"m", "M"}, {"n", "N"}, {"o", "O"}, {"p", "P"},
         {"q", "Q"}, {"r", "R"}, {"s", "S"}, {"t", "T"},
         {"u", "U"}, {"v", "V"}, {"w", "W"}, {"x", "X"},
-        {"y", "Y"}, {"z", "Z"}, {"1", "!"}, {"2", "@"}, 
+        {"y", "Y"}, {"z", "Z"}, {"1", "!"}, {"2", "@"},
         {"3", "#"}, {"4", "$"}, {"5", "%"}, {"6", "^"},
         {"7", "&"}, {"8", "*"}, {"9", "("}, {"0", ")"},
         {"-", "_"}, {"=", "+"}, {"[", "{"}, {"]", "}"},
         {"\\", "|"}, {";", ":"}, {"'", "\""}, {",", "<"},
         {".", ">"}, {"/", "?"}
     };
-    
+
 
     public void typingFunct(string letter) {
         //check if the character is not in the dictionary
--- CapstoneEscapeRoom/Assets/Scripts/Terminal.cs
+++ CapstoneEscapeRoom/Assets/Scripts/Terminal.cs
@@ -4,62 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Terminal : MonoBehaviour {
+public class Terminal : KeyboardTyping {
 
-    string word = null;
-    int wordIndex = 0;
-    public TMP_Text output = null;
-    bool shift = false;
-    IDictionary<string, string> shiftDictionary = new Dictionary<string, string>() {
-        {"a", "A"}, {"b", "B"}, {"c", "C" }, {"d", "D"}, 
-        {"e", "E"}, {"f", "F"}, {"g", "G"}, {"h", "H"},
-        {"i", "I"}, {"j", "J" }, {"k", "K"}, {"l", "L"},
-        {"m", "M"}, {"n", "N"}, {"o", "O"}, {"p", "P"},
-        {"q", "Q"}, {"r", "R"}, {"s", "S"}, {"t", "T"},
-        {"u", "U"}, {"v", "V"}, {"w", "W"}, {"x", "X"},
-        {"y", "Y"}, {"z", "Z"}, {"1", "!"}, {"2", "@"}, 
-        {"3", "#"}, {"4", "$"}, {"5", "%"}, {"6", "^"},
-        {"7", "&"}, {"8", "*"}, {"9", "("}, {"0", ")"},
-        {"-", "_"}, {"=", "+"}, {"[", "{"}, {"]", "}"},
-        {"\\", "|"}, {";", ":"}, {"'", "\""}, {",", "<"},
-        {".", ">"}, {"/", "?"}
+    //wordIndex, word, and output are defined in the parent class
+    public string commandLine = null;
+    public string user = "C:\\Users\\Champ> ";
+
+    //a dictionary of commands and their outputs
+    IDictionary<string, string> commands = new Dictionary<string, string>() {
+        {"ls", "tmp.txt \t passwords.txt \t forms \n" +
+                "user_information \t taxes.exe" }
     };
 
-
-    public void typingFunct(string letter) {
-        //check if the character is not in the dictionary
-        //or shift is false
-        if (!shiftDictionary.ContainsKey(letter) | shift == false) {
-            wordIndex++;
-            word = word + letter;
-            output.text = word; // send to text mesh pro
-        }
-        else if (shift == true) {
-            wordIndex++;
-            word = word + shiftDictionary[letter];
-            output.text = word;
-        }
-        shift = false;
-    }
-
-    public void backspaceFunct() {
-        //only erase a character if there are characters to erase
-        if (wordIndex > 0) {
-            //subtract a character from the word
-            word = word.Substring(0, wordIndex - 1);
-            wordIndex--;
-            output.text = word;
-        }
-    }
-
-    public void shiftFunct() {
-        shift = true;
-    }
-
-    public void terminalFunct() {
-        //wait one second
-        //System.Threading.Thread.Sleep(1000);
-        word = "Testing!!";
+    public void startTerminal() {
+        word += user;
         output.text = word;
+    }
+
+    public void commandExecution() {
+        //check for the command
+        if (commands.ContainsKey(word)) {
+            textCrawler("\n" + commands[word]);
+        }
+        //give an error if that command is not valid
+        else {
+            textCrawler("\n\'" + word + "\' is not recognized as an internal or external command");
+        }
+        //reset terminal input
+        word = "";
+        wordIndex = 0;
+    }
+
+    //send text to the terminal one letter at a time
+    public void textCrawler(string text) {
+        for (int i = 0; i < text.Length; i++) {
+
+            word += text[i];
+            //System.Threading.Thread.Sleep(500);
+            output.text = word;
+        }
     }
 }
