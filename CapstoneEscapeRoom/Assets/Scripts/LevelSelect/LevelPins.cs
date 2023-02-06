0476ba7723293a88c29bef0ba7f8ce50df792e87
Ty Pederson <62142901+TyPe23@users.noreply.github.com>
2023-02-05 18:19:00 -0600

    button updates

--- CapstoneEscapeRoom/Assets/Scripts/LevelSelect/LevelPins.cs
+++ CapstoneEscapeRoom/Assets/Scripts/LevelSelect/LevelPins.cs
@@ -12,22 +12,17 @@ public class LevelPins : MonoBehaviour
     public GameObject button;
     public GameObject[] buttons;
     public UnityEvent onPress, onReleased;
-    bool isPressed = false;
 
-    public void OnTriggerEnter(Collider other) // for button on level selection 
+    public void LevelSet()
     {
-        if (!isPressed) // check if pressed 
+        for (int i = 0; i < buttons.Length; i++)
         {
-            for (int i = 0; i< buttons.Length; i++)
-            {
-                buttons[i].SetActive(true);
-            }
-
-            onPress.Invoke();
-            isPressed = true; // changed that it is pressed 
-            Debug.Log("Button Pressed");
-
-            button.SetActive(false);
+            buttons[i].SetActive(true);
         }
+
+        Debug.Log("Button Pressed");
+
+        button.SetActive(false);
     }
+
 }
