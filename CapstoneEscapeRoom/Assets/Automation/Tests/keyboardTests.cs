959dcf1d08c40ab4d7fa987e7a561a26ded7b420
MarydeM <maryevelyndemarigny@gmail.com>
2023-02-06 19:25:44 -0600

    basic Terminal unit test is working

---
+++ CapstoneEscapeRoom/Assets/Automation/Tests/keyboardTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TrilleonAutomation {
+
+    [AutomationClass]
+    public class TerminalTests : MonoBehaviour {
+
+        [SetUpClass]
+        public IEnumerator SetUpClass() {
+
+            yield return null;
+
+        }
+
+        [SetUp]
+        public IEnumerator SetUp() {
+
+            yield return null;
+
+        }
+
+        [Automation("Terminal")]
+        public IEnumerator TerminalTest() {
+
+            GameObject gameObject = null;
+            gameObject = Q.driver.Find(By.Name, "ComputerScreen");
+            yield return StartCoroutine(Q.driver.Click(Q.driver.FindIn(gameObject, By.Name, "TerminalIcon", false), "Click object with name TerminalIcon"));
+            //yield return StartCoroutine(Q.assert.IsTrue(false, "This will fail.")); 
+            //yield return null;
+
+        }
+
+        [TearDown]
+        public IEnumerator TearDown() {
+
+            yield return null;
+
+        }
+
+        [TearDownClass]
+        public IEnumerator TearDownClass() {
+
+            yield return null;
+
+        }
+
+    }
+
+}
