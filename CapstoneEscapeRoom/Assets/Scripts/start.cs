ea458d7870433f7036a73613403c3c00f2a089c2
MatthewT <61696310+firerexman007@users.noreply.github.com>
2023-02-12 15:53:39 -0600

    Test/Fix

--- CapstoneEscapeRoom/Assets/Scripts/start.cs
+++ CapstoneEscapeRoom/Assets/Scripts/start.cs
@@ -9,9 +9,16 @@ using UnityEngine.UI;
 public class Start{
 	static Start(){
         //calls the Trilleon automatic tester if Debug is on
+<<<<<<< Updated upstream
         if (Application.isEditor || Debug.isDebugBuild) {
             TrilleonAutomation.AutomationMaster.Initialize();
         }
         Main test = new Main();
+=======
+        //if (Application.isEditor || Debug.isDebugBuild) {
+            //TrilleonAutomation.AutomationMaster.Initialize();
+        //}
+        //Main test = new Main();
+>>>>>>> Stashed changes
     }
 }
