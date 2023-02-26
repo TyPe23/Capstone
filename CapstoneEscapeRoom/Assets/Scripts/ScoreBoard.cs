6bc53d78ddb5bbb11142074e9a2239eae772b3a4
MarydeM <maryevelyndemarigny@gmail.com>
2023-02-26 13:17:41 -0600

    changed the start in ScoreBoard.cs to call Database.start()

--- CapstoneEscapeRoom/Assets/Scripts/ScoreBoard.cs
+++ CapstoneEscapeRoom/Assets/Scripts/ScoreBoard.cs
@@ -15,6 +15,7 @@ public class ScoreBoard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Database.start();
         string header =
                     "\t Level _\n" +
                     "------------------------------\n";
