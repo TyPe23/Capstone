88147e987cff9c5b58f14b2b48fe88683b10b05b
BenjaminJSanguinetti <BenjaminJSanguinetti@gmail.com>
2023-03-16 14:34:15 -0500

    bonus points scripts update

---
+++ CapstoneEscapeRoom/Assets/Scripts/BonusPoints.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusPoints : MonoBehaviour
+{
+    public int bPoints = 0;
+    public int pointsPer = 500;
+    //PlayerPrefs.SetInt("BonusPoints", 0);
+    public bool[] items = { false, false, false, false, false };
+
+    public void updatePoints(int i)
+    {
+        if(items[i])
+        {
+            try
+            {
+                bPoints = PlayerPrefs.GetInt("BonusPoints");
+            }
+            catch 
+            { 
+                bPoints = 0;
+            }
+            bPoints += pointsPer;
+            PlayerPrefs.SetInt("BonusPoints", bPoints);
+            items[i] = true;
+        }
+    }
+       
+
+
+}
