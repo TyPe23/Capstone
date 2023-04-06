b11dcfc996942416ce39d9c1dd5a1e6207e25496
MatthewT <61696310+firerexman007@users.noreply.github.com>
2023-04-06 00:28:26 -0500

    Port Creation

---
+++ CapstoneEscapeRoom/Assets/Scripts/RandomPorts.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPorts : MonoBehaviour
+{
+    // all the port objects 
+    public GameObject[] allPorts;
+    // empty arry of vector 3s 
+    private Vector3[] locations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        locations = new Vector3[allPorts.Length]; // set length of locations
+        // get locations of all ports 
+        int i = 0;
+        foreach(GameObject port in allPorts)
+        {
+            locations[i]= port.transform.position;
+            i++;
+        }
+        List<int> used = new List<int>();
+        int x = 0;
+        bool c = true;
+        foreach(GameObject port in allPorts)
+        {
+            c = true;
+            while (c)
+            {
+                x = Random.Range(0, allPorts.Length);
+                if (!(used.Contains(x)))
+                {
+                    port.transform.position = locations[x];
+                    c = false;
+                    used.Add(x);
+                }
+            }
+            
+        }
+
+    }
+
+}
