6e365f8fa8fac2002ab9471dd8ba3c69fcbf4547
MatthewT <61696310+firerexman007@users.noreply.github.com>
2023-04-20 23:01:10 -0500

    Randomization and fixes

--- CapstoneEscapeRoom/Assets/Scripts/Other/MoveChanger.cs
+++ CapstoneEscapeRoom/Assets/Scripts/Other/MoveChanger.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+// Matthew Tucker 
+// Date: 4/20/23
+//Description: Toggle to switch between continous and teleporting movment 
 public class MoveChanger : MonoBehaviour
 {
     // current movment 
     public bool continuousMovment = true;
     public bool telportMovment = false;
-    // gameObjects 
+    // movement system
     public ActionBasedContinuousTurnProvider continuousMove1;
     public ActionBasedContinuousMoveProvider continuousMove2;
-    public ActionBasedSnapTurnProvider telportMove1;
+    //public ActionBasedSnapTurnProvider telportMove1;
     public TeleportationProvider telportMove2;
-
+    // left hand collider for when teleporting to prevent handing going though wall 
+    public BoxCollider LeftHandCollider;
+    public GameObject LeftHand;
+    // defaults and settings 
     public XRInteractorLineVisual line;
     public float defaultLength = 0.5f;
     public XRRayInteractor linePart;
@@ -24,7 +30,7 @@ public class MoveChanger : MonoBehaviour
         
     }
 
-    // chaning
+    // chaning systyem 
     public void changMovemnet()
     {
         if (continuousMovment)
@@ -36,31 +42,37 @@ public class MoveChanger : MonoBehaviour
             linePart.lineType = XRRayInteractor.LineType.ProjectileCurve;
             linePart.velocity = 5;
             linePart.interactionLayers = LayerMask.GetMask("Default");
+            LeftHand.layer = LayerMask.GetMask("Default");
         }
         else
         {
-            continuousMovment = true;
-            telportMovment = false;
+            continuousMovment = false;
+            telportMovment = true;
             continMoving();
             line.lineLength = defaultLength;
             linePart.lineType = XRRayInteractor.LineType.StraightLine;
             linePart.interactionLayers = ~0;
+            LeftHand.layer = LayerMask.GetMask("Player");
         }
     }
 
+    // change to continous moving
     void continMoving()
     {
         continuousMove1.enabled = true;
         continuousMove2.enabled = true;
-        telportMove1.enabled = false;
+        //telportMove1.enabled = false;
         telportMove2.enabled = false;
+        LeftHandCollider.enabled = false;
     }
 
+    // change to tel moving 
     void telMoving()
     {
         continuousMove1.enabled = false;
         continuousMove2.enabled = false;
-        telportMove1.enabled = true;
+        //telportMove1.enabled = true;
         telportMove2.enabled = true;
+        LeftHandCollider.enabled |= true;
     }
 }
