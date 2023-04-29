31d5a4e691dc8d17a5439c6206006a0a10b7d0a3
BenjaminJSanguinetti <BenjaminJSanguinetti@gmail.com>
2023-04-29 12:01:09 -0500

    hnts fx

--- CapstoneEscapeRoom/Assets/Scripts/Player/Hints.cs
+++ CapstoneEscapeRoom/Assets/Scripts/Player/Hints.cs
@@ -82,7 +82,7 @@ public class Hints : MonoBehaviour
                 switch (task)
                 {
                     case 3:
-                        output = "the key card can be found in the Cabent";
+                        output = "the key card can be found in the cabnet";
                         break;
                     case 2:
                         output = "find blinking port and plug into computer with cable";
